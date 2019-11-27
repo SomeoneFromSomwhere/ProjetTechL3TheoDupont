c0e4dbbb9f6cf5af95b65236c034db59637b51ce
Théo Dupont <theo.dupont@etu.u-bordeaux.fr>
2019-11-27 19:04:05 +0100

    Probeleme Renderscript

---
+++ app/src/main/rs/gray2.rs
@@ -0,0 +1,13 @@
+#pragma version(1)
+#pragma rs java_package_name ( com.example.hello)
+
+static const float4 weight = {0.299 f , 0.587 f , 0.114 f , 0.0 f };
+
+uchar4 RS_KERNEL toGray2 ( uchar4 in ) {
+
+    const float4 pixelf = rsUnpackColor8888 ( in ) ;
+    const float gray = dot ( pixelf , weight ) ;
+
+    return rsPackColorTo8888 ( gray , gray , gray , pixelf . a ) ;
+
+}
